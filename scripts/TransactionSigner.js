0c9ee3f7a2ad49fa98f8bd7600da5730dd05cf72
win2win <win2win@protonmail.com>
2022-07-06 08:21:04 -0500

    adjust ports

--- scripts/TransactionSigner.js
+++ scripts/TransactionSigner.js
@@ -144,8 +144,7 @@ app.get('/sign', async function (req, res) {
     }
 })
 
-
-let server = app.listen(3336, function () {
+let server = app.listen(3333, function () {
     let host = server.address().address
     let port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
